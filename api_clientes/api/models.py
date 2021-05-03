9ef4d4022fc16f2b7dde93127d76145799ce6772
Karlos <karlos.braga@inmetrics.com.br>
2021-05-03 12:01:39 -0300

    Finished validate

--- api_clientes/api/models.py
+++ api_clientes/api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import requests
- 
+
+
+# Classe Tbl_Clientes vira uma tabela no MySQL
 class Tbl_Clientes(models.Model):
     nome_completo_cliente = models.CharField(max_length=200)
     cpf = models.CharField(max_length=15)
@@ -8,10 +10,15 @@ class Tbl_Clientes(models.Model):
     data_nascimento = models.DateField()
     numero_telefone = models.CharField(max_length=15)
     deletado = models.BooleanField()
-
+    
+    '''
+    Se for feito pedido um texto representativo
+    do cliente, será retornado o nome (string).
+    '''
     def __str__(self):
         return self.nome_completo_cliente
 
+# Classe Tbl_Enderecos vira uma tabela no MySQL
 class Tbl_Enderecos(models.Model):
     rua = models.CharField(max_length=200)
     numero = models.CharField(max_length=10)
@@ -20,13 +27,21 @@ class Tbl_Enderecos(models.Model):
     cidade = models.CharField(max_length=200)
     estado = models.CharField(max_length=200)
     pais = models.CharField(max_length=200)
-    cliente_id = cliente_id = models.IntegerField()
+    cliente_id = models.IntegerField()
     deletado = models.BooleanField()
 
+    '''
+    Uma função que busca os dados dos clientes na API Clientes
+    através do ID.
+    '''
     @property
     def cliente(self):
         url_cliente = "http://localhost:8000/clientes_api/" + str(self.cliente_id) + "/"
         return requests.get(url_cliente, auth=('admin','admin')).json()
 
+    '''
+    Se for feito pedido um texto representativo
+    do endereço, será retornado a rua e o número (strings).
+    '''
     def __str__(self):
         return self.rua + ', ' + self.numero
--- api_clientes/api/serializers.py
+++ api_clientes/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import Tbl_Clientes, Tbl_Enderecos
 from validate_docbr import CPF
+import re
 
+'''
+Essa classe transforma os JSONs recebidos em objetos
+na memória e vice-versa.
+'''
 class Tbl_ClientesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tbl_Clientes
@@ -15,6 +20,32 @@ class Tbl_ClientesSerializer(serializers.ModelSerializer):
             'deletado',
         ]
     
+    def validate_cpf(self, value):
+        cpf = CPF()
+        if not cpf.validate(value):
+            raise serializers.ValidationError(
+                'Não é um CPF válido!'
+            )
+        return value
+    
+    def validate_email(self, value):
+        if not re.match('.+@.+\..+', value):
+            raise serializers.ValidationError(
+                'Não é um e-mail válido!'
+            )
+        return value
+
+    def validate_numero_telefone(self, value):
+        if not re.match('\(\d{2,3}\) ?\d{4,5}\-\d{4}', value):
+            raise serializers.ValidationError(
+                'Não é um telefone válido!'
+            )
+        return value
+
+'''
+Essa classe transforma os JSONs recebidos em objetos
+na memória e vice-versa.
+'''
 class Tbl_EnderecosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tbl_Enderecos
@@ -30,8 +61,19 @@ class Tbl_EnderecosSerializer(serializers.ModelSerializer):
             'cliente',
             'deletado',
         ]
-        depth = 1
     
+    def validate_cep(self, value):
+        if not re.match('\d{5}\-\d{3}', value):
+            raise serializers.ValidationError(
+                'Não é um CEP válido!'
+            )
+        return value
+    
+    '''
+    Função que verifica se o ID do cliente existe
+    e não pertence a um cliente deletado para inserir
+    ou atualizar um endereço.
+    '''
     def validate_cliente_id(self, cli_id):
         cliente = Tbl_Clientes.objects.get(pk = cli_id)
         if cliente.deletado:
